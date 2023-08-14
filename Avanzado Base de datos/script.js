d1274e66df224af40602deb492cf5a9f95db4a44
Dantemacu <dantemacu@gmail.com>
2023-08-14 14:21:26 -0300

    added(Termine la animaion de la barra de tareas, falta pulirlo y que quede clean)

---
+++ Avanzado Base de datos/script.js
@@ -0,0 +1,73 @@
+const alumnos = [
+{
+    nombre: "Juan",
+    apellido: "Gomez",
+    dni: 47000000,
+    anio: 4,
+    curso: "A",
+    nota: 8,
+},
+
+{
+    nombre: "Juan",
+    apellido: "Perez",
+    dni: 47000001,
+    anio: 4,
+    curso: "A",
+    nota: 9,
+},
+
+{
+    nombre: "Dante",
+    apellido: "Maculan",
+    dni: 47000002,
+    anio: 5,
+    curso: "A",
+    nota: 10,
+},
+
+{
+    nombre: "Martin",
+    apellido: "Berestovoy",
+    dni: 47000003,
+    anio: 5,
+    curso: "A",
+    nota: 1,
+},
+
+{
+    nombre: "Ramiro",
+    apellido: "Levi",
+    dni: 47000004,
+    anio: 4,
+    curso: "B",
+    nota: 2,
+},
+
+{
+    nombre: "Bautista",
+    apellido: "Czerniuk",
+    dni: 47000005,
+    anio: 4,
+    curso: "B",
+    nota: 3,
+},
+
+{
+    nombre: "Santino",
+    apellido: "Couriel",
+    dni: 47000006,
+    anio: 3,
+    curso: "B",
+    nota: 5,
+},
+
+{
+    nombre: "Zoe",
+    apellido: "Perez Colman",
+    dni: 47000007,
+    anio: 3,
+    curso: "B",
+    nota: 8,
+},
+]
---
+++ RigIA/Home.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const openButton = document.getElementById("barra-opn");
+    const closeButton = document.getElementById("barra-close");
+    const navBar = document.querySelector(".barra-options");
+
+    openButton.addEventListener("click", function() {
+        navBar.classList.add("open");
+    });
+
+    closeButton.addEventListener("click", function() {
+        navBar.classList.remove("open");
+    });
+});

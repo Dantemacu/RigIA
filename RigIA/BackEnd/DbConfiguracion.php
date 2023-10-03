ed3b05013e27b2c5179e6cf34e4fcb3ca6cdec1d
JoaquinDesimone <47650971@est.ort.edu.ar>
2023-10-03 08:48:23 -0300

    .

--- RigIA/BackEnd/DbConfiguracion.php
+++ RigIA/BackEnd/DbConfiguracion.php
@@ -1,9 +1,10 @@
-<?php
-//var_dump ($_POST);
-$nombre= $_POST;
-
+<?php 
 $_ENV = parse_ini_file('.env');
 $mysqli = mysqli_init();
 $mysqli->ssl_set(NULL, NULL, "./cacert.pem", NULL, NULL);
-$mysqli->real_connect($_ENV["HOST"], $_ENV["USERNAME"], $_ENV["PASSWORD"], $_ENV["NAME"]);
+$mysqli->real_connect($_ENV["DB_HOST"], $_ENV["DB_USERNAME"], $_ENV["DB_PASSWORD"], $_ENV["DB_NAME"]);
+
+if ($mysqli->connect_error) {
+    die("Error de conexión: " . $mysqli->connect_error);
+}
 ?>
--- RigIA/BackEnd/SignUpSignIn.php
+++ RigIA/BackEnd/SignUpSignIn.php
@@ -1,6 +1,92 @@
 <?php
-/*require 'DbConfiguracion.php';
+require 'DbConfiguracion.php';
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login y Registro</title>
+    <link rel="stylesheet" href="SignUpSignIn.css">
+    <script src="https://kit.fontawesome.com/a6f001e2da.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container">
+        <div class="form-box">
+            <h1 id="title">Sign Up</h1>
+            <form id="authForm" action="" method="POST">
+            <input type="hidden" name="form_type" id="form_type" value=""> <!-- Valor "register" para el formulario de registro -->
+                <div class="input-group">
+                    <div class="input-field" id="nameField">
+                        <i class="fas fa-user"></i>
+                        <input type="text" name="Nombre" id="nameInput" placeholder="Nombre">
+                    </div>
+
+                    <div class="input-field">
+                        <i class="fas fa-envelope"></i>
+                        <input type="email" name="Mail" id="emailInput" placeholder="Email">
+                    </div>
+
+                    <div class="input-field">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" name="Contrasenia" id="passwordInput" placeholder="Contraseña">
+                    </div>
+                    <p>¿Olvidaste la contraseña? <a href="#">Presiona aquí</a></p>
+                </div>
+                <div class="input-submit">
+                    <button type="submit" name="submitButton" class="submit" id="Submit">
+                        Ingresar
+                    </button>
+                </div>
+                <div class="btn-field">
+                    <button type="button" id="signupBtn"> Registrarse</button>
+                    <button type="button" id="signinBtn" class="disable"> Iniciar Sesión</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <script>
+        let signupBtn = document.getElementById("signupBtn");
+        let signinBtn = document.getElementById("signinBtn");
+        let nameField = document.getElementById("nameField");
+        let title = document.getElementById("title");
+        let Submit = document.getElementById("Submit");
+        let authForm = document.getElementById("authForm");
+        let nameInput = document.getElementById("nameInput");
+        let passwordInput = document.getElementById("passwordInput");
+        const submit = document.getElementById("Submit");
+
+        signinBtn.onclick = function() {
+            nameField.style.maxHeight = "0";
+            title.innerHTML = "Iniciar Sesión";
+            signupBtn.classList.remove("disable");
+            signinBtn.classList.add("disable");
+            Submit.innerHTML = "Ingresar";
+            //authForm.setAttribute("action", "SignUpQuery.php"); // Cambiar la acción del formulario para el inicio de sesión
+            nameInput.setAttribute("name", "Nombre2"); // Cambiar el atributo name del input de nombre
+            passwordInput.setAttribute("name", "Contrasenia2"); // Cambiar el atributo name del input de contraseña
+            document.getElementById("form_type").value = "login";  
+        }
+
+
+        signupBtn.onclick = function() {
+            nameField.style.maxHeight = "60px";
+            title.innerHTML = "Registrarse";
+            signupBtn.classList.add("disable");
+            signinBtn.classList.remove("disable");
+            Submit.innerHTML = "Registrarse";
+            //authForm.setAttribute("action", "signup-action.php"); // Cambiar la acción del formulario para el registro
+            nameInput.setAttribute("name", "Nombre"); // Restaurar el atributo name del input de nombre
+            passwordInput.setAttribute("name", "Contrasenia"); // Restaurar el atributo name del input de contraseña
+            document.getElementById("form_type").value = "register";
+        }
+    </script>
+</body>
+</html>
+
+<?php
 // Verificar si se ha enviado un formulario
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     if (isset($_POST["form_type"])) {
@@ -70,88 +156,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         }
     }
 }
-*/
+
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login y Registro</title>
-    <link rel="stylesheet" href="SignUpSignIn.css">
-    <script src="https://kit.fontawesome.com/a6f001e2da.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="container">
-        <div class="form-box">
-            <h1 id="title">Sign Up</h1>
-            <form id="authForm" action="" method="POST">
-            <input type="hidden" name="form_type" id="form_type" value=""> <!-- Valor "register" para el formulario de registro -->
-                <div class="input-group">
-                    <div class="input-field" id="nameField">
-                        <i class="fas fa-user"></i>
-                        <input type="text" name="Nombre" id="nameInput" placeholder="Nombre">
-                    </div>
 
-                    <div class="input-field">
-                        <i class="fas fa-envelope"></i>
-                        <input type="email" name="Mail" id="emailInput" placeholder="Email">
-                    </div>
-
-                    <div class="input-field">
-                        <i class="fas fa-lock"></i>
-                        <input type="password" name="Contrasenia" id="passwordInput" placeholder="Contraseña">
-                    </div>
-                    <p>¿Olvidaste la contraseña? <a href="#">Presiona aquí</a></p>
-                </div>
-                <div class="input-submit">
-                    <button type="submit" name="submitButton" class="submit" id="Submit">
-                        Ingresar
-                    </button>
-                </div>
-                <div class="btn-field">
-                    <button type="button" id="signupBtn"> Registrarse</button>
-                    <button type="button" id="signinBtn" class="disable"> Iniciar Sesión</button>
-                </div>
-            </form>
-        </div>
-    </div>
-
-    <script>
-        let signupBtn = document.getElementById("signupBtn");
-        let signinBtn = document.getElementById("signinBtn");
-        let nameField = document.getElementById("nameField");
-        let title = document.getElementById("title");
-        let Submit = document.getElementById("Submit");
-        let authForm = document.getElementById("authForm");
-        let nameInput = document.getElementById("nameInput");
-        let passwordInput = document.getElementById("passwordInput");
-
-        signinBtn.onclick = function() {
-            nameField.style.maxHeight = "0";
-            title.innerHTML = "Iniciar Sesión";
-            signupBtn.classList.remove("disable");
-            signinBtn.classList.add("disable");
-            Submit.innerHTML = "Ingresar";
-            //authForm.setAttribute("action", "SignUpQuery.php"); // Cambiar la acción del formulario para el inicio de sesión
-            nameInput.setAttribute("name", "Nombre2"); // Cambiar el atributo name del input de nombre
-            passwordInput.setAttribute("name", "Contrasenia2"); // Cambiar el atributo name del input de contraseña
-            document.getElementById("form_type").value = "login";  
-        }
-
-
-        signupBtn.onclick = function() {
-            nameField.style.maxHeight = "60px";
-            title.innerHTML = "Registrarse";
-            signupBtn.classList.add("disable");
-            signinBtn.classList.remove("disable");
-            Submit.innerHTML = "Registrarse";
-            //authForm.setAttribute("action", "signup-action.php"); // Cambiar la acción del formulario para el registro
-            nameInput.setAttribute("name", "Nombre"); // Restaurar el atributo name del input de nombre
-            passwordInput.setAttribute("name", "Contrasenia"); // Restaurar el atributo name del input de contraseña
-            document.getElementById("form_type").value = "register";
-        }
-    </script>
-</body>
-</html>

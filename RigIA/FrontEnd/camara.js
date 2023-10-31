f7296129a886bdca74bd3d4859cfff0a889c3ff4
Dantemacu <dantemacu@gmail.com>
2023-10-31 10:35:06 -0300

    Cambios camara accion

--- RigIA/FrontEnd/camara.js
+++ RigIA/FrontEnd/camara.js
@@ -140,6 +140,9 @@
 
       const data = canvas.toDataURL("image/png");
       photo.setAttribute("src", data);
+
+      var photoName = 'captura_' + new Date().getTime() + '.png'; 
+      console.log(photoName);
     } else {
       clearphoto();
     }
